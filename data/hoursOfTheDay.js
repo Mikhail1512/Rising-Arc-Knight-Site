bc982db61c58f487b14ef11756dfdce9c5ce392b
Mikhail1512 <mikhailm071512@gmail.com>
2025-04-05 16:01:12 +0200

    Created and generate update card with timeTask info and addnew ocjects for Timed Tasks

--- data/hoursOfTheDay.js
+++ data/hoursOfTheDay.js
@@ -1,9 +1,20 @@
+/*
+    To-Do List 
+        : turn taskText into an object to contain the title, description and experience points 
+        
+*/ 
+
 export const hoursOfTheDays = [
     {
         id: "first-hour-00-00-00-morning",
         startTime: "00:00",
         endTime: "00:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -12,7 +23,12 @@ export const hoursOfTheDays = [
         id: "second-hour-01-00-00-morning",
         startTime: "01:00",
         endTime: "01:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -21,7 +37,12 @@ export const hoursOfTheDays = [
         id: "third-hour-02-00-00-morning",
         startTime: "02:00",
         endTime: "02:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -30,7 +51,12 @@ export const hoursOfTheDays = [
         id: "fourth-hour-03-00-00-morning",
         startTime: "03:00",
         endTime: "03:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -39,7 +65,13 @@ export const hoursOfTheDays = [
         id: "fifth-hour-04-00-00-morning",
         startTime: "04:00",
         endTime: "04:59",
-        taskText: '',
+        timedTask: {
+            id: '02-freshenUp',
+            titleText: 'Fresh up rountine',
+            descriptiveText: 'Make sure to brush you teeth, wash and condition your hair, perform wudhu also shave your beard and clip your nails',
+            experiencePoints: 25,
+        },
+
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -48,7 +80,12 @@ export const hoursOfTheDays = [
         id: "sixth-hour-05-00-00-morning",
         startTime: "05:00",
         endTime: "05:59",
-        taskText: '',
+        timedTask: {
+            id: '03-taskGym',
+            titleText: 'Gym',
+            descriptiveText: 'Warm up a bit, check your workout schedule for the order of exercises you should perform ',
+            experiencePoints: 150,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -57,7 +94,12 @@ export const hoursOfTheDays = [
         id: "seventh-hour-06-00-00-morning",
         startTime: "06:00",
         endTime: "06:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskShower',
+            titleText: 'Shower after gyming',
+            descriptiveText: 'Finish gyming so you can hope in the shower',
+            experiencePoints: 40,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -66,7 +108,12 @@ export const hoursOfTheDays = [
         id: "eighth-hour-07-00-00-morning",
         startTime: "07:00",
         endTime: "07:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -75,7 +122,12 @@ export const hoursOfTheDays = [
         id: "ninth-hour-08-00-00-morning",
         startTime: "08:00",
         endTime: "08:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskStretch',
+            titleText: 'Strecthing rountine',
+            descriptiveText: 'Check you workout plan for your rountine',
+            experiencePoints: 60,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -84,7 +136,12 @@ export const hoursOfTheDays = [
         id: "tenth-hour-09-00-00-morning",
         startTime: "09:00",
         endTime: "09:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -93,7 +150,12 @@ export const hoursOfTheDays = [
         id: "eleventh-hour-10-00-00-morning",
         startTime: "10:00",
         endTime: "10:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -102,7 +164,12 @@ export const hoursOfTheDays = [
         id: "twelveth-hour-11-00-00-morning",
         startTime: "11:00",
         endTime: "11:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -111,7 +178,12 @@ export const hoursOfTheDays = [
         id: "thirteenth-hour-12-00-00-afternoon",
         startTime: "12:00",
         endTime: "12:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -120,7 +192,12 @@ export const hoursOfTheDays = [
         id: "fourteenth-hour-13-00-00-afternoon",
         startTime: "13:00",
         endTime: "13:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -129,7 +206,12 @@ export const hoursOfTheDays = [
         id: "fifteenth-hour-14-00-00-afternoon",
         startTime: "14:00",
         endTime: "14:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -138,7 +220,12 @@ export const hoursOfTheDays = [
         id: "sixteenth-hour-15-00-00-afternoon",
         startTime: "15:00",
         endTime: "15:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -147,7 +234,12 @@ export const hoursOfTheDays = [
         id: "seventeenth-hour-16-00-00-afternoon",
         startTime: "16:00",
         endTime: "16:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskCardio',
+            titleText: 'Hit some cardio',
+            descriptiveText: 'Pick an exercise to do between running, cycling or walking if it is the first week',
+            experiencePoints: 40,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -156,7 +248,12 @@ export const hoursOfTheDays = [
         id: "eighteenth-hour-17-00-00-afternoon",
         startTime: "17:00",
         endTime: "17:59",
-        taskText: '',
+        taskText: 'Shower',timedTask: {
+            id: '01-taskShower',
+            titleText: 'Shower after cardio',
+            descriptiveText: 'Kick it to the shower after doing some cardio',
+            experiencePoints: 200,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -165,7 +262,12 @@ export const hoursOfTheDays = [
         id: "ninteenth-hour-18-00-00-night",
         startTime: "18:00",
         endTime: "18:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSideQuest',
+            titleText: 'Side Quest Studies',
+            descriptiveText: 'Check your list of side quest list for skills to train',
+            experiencePoints: 100,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -174,7 +276,12 @@ export const hoursOfTheDays = [
         id: "twentyth-hour-19-00-00-night",
         startTime: "19:00",
         endTime: "19:59",
-        taskText: '',
+        timedTask: {
+            id: '',
+            titleText: '',
+            descriptiveText: '',
+            experiencePoints: 0,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -183,7 +290,12 @@ export const hoursOfTheDays = [
         id: "twentyfirst-hour-20-00-00-night",
         startTime: "20:00",
         endTime: "20:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskChill',
+            titleText: 'Chill out',
+            descriptiveText: 'Chill in bed and try to fall asleep',
+            experiencePoints: 5,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -192,7 +304,12 @@ export const hoursOfTheDays = [
         id: "twentysecond-hour-21-00-00-night",
         startTime: "21:00",
         endTime: "21:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'fallingAsleeping',
+            descriptiveText: 'Try to fall asleep on time and set an alarm dude, maybe even drink a bunch of ',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -201,7 +318,12 @@ export const hoursOfTheDays = [
         id: "twentythird-hour-22-00-00-night",
         startTime: "22:00",
         endTime: "22:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
@@ -210,7 +332,12 @@ export const hoursOfTheDays = [
         id: "twentyfourth-hour-23-00-00-night",
         startTime: "23:00",
         endTime: "23:59",
-        taskText: '',
+        timedTask: {
+            id: '01-taskSleep',
+            titleText: 'Sleeping',
+            descriptiveText: 'You should sleeping well inorder to recover and wake up in time',
+            experiencePoints: 10,
+        },
         activeTime: false,
         isUpdating: false,
         isRemoving: false
--- scripts/calender.js
+++ scripts/calender.js
@@ -1,3 +1,18 @@
+/* 
+ To-do list 
+    - make button appear upon clicking the time slot
+    - add function to the update and reomve button 
+    - create dates of the month calender
+    - create an hourly slot detail card for updating and removing tasks
+
+
+ Ideas 
+    : make a eplises button for the detail card 
+    : make a quick clear button
+    : try to make a permenent and once off remove and update choice
+    : have a task history for changes to be undone
+*/
+
 import { generateHeader } from "./header.js";
 import { hoursOfTheDays } from "../data/hoursOfTheDay.js";
 
@@ -9,16 +24,18 @@ function generateHourlyTimeTable() {
     let timeTableHTML = '';
 
     hoursOfTheDays.forEach((hourlyInfo) => {
-        hourlyInfo.taskText = 'Sleep';
+        if(hourlyInfo.timedTask.titleText === ''){
+            hourlyInfo.timedTask.titleText = '- - -';
+        }
 
         timeTableHTML += `
             <div class="time-table-hours">
-                <div class="hour-task-texts js-hour-task-text">${hourlyInfo.taskText}</div>
+                <div class="hour-task-texts js-hour-task-text-${hourlyInfo.id}">${hourlyInfo.timedTask.titleText}</div>
                 <div class="bottom-row-hour-container">
                     <div class="js-hour-time-display">${hourlyInfo.startTime} - ${hourlyInfo.endTime}</div>
                     <div class="hours-button-container">
-                        <button class="js-hour-update-button">Update</button>
-                        <button class="js-hour-remove-button">Remove</button>
+                        <button class="hour-update-button js-hour-update-button-${hourlyInfo.id}">Update</button>
+                        <button class="hour-clear-button js-hour-clear-button">Clear</button>
                     </div>
                 </div>
             </div>
@@ -32,3 +49,65 @@ function generateHourlyTimeTable() {
 generateHourlyTimeTable();
 
 
+
+
+function updateTimedTasks(){
+    hoursOfTheDays.forEach((hourlyInfo) => {
+        document.querySelector(`.js-hour-update-button-${hourlyInfo.id}`).addEventListener('click',() => {
+            // hourlyInfo.timedTask.titleText = "Not Sleeping";
+            // console.log(hourlyInfo.timedTask.titleText);
+            // document.querySelector(`.js-hour-task-text-${hourlyInfo.id}`).innerHTML = hourlyInfo.timedTask.titleText;
+            controlUpdateGeneration(hourlyInfo);
+        });
+    });
+}
+
+updateTimedTasks();
+
+
+function controlUpdateGeneration(hourlyInfo){
+    const updateHTML = `
+        <div class="updating-info-container">
+            <div class="updating-card">
+                <div class="updating-card-header">
+                    <span>Enter new task and confirm</span>
+                    <span>⚙️</span>
+                </div>
+                <div class="title-container">
+                    <label>Task Title:</label>
+                    <input type="text" class="update-input-title-field" value="${hourlyInfo.timedTask.titleText}">
+                </div>
+                <div class="description-container">
+                    <label>Task Description:</label>
+                    <input class="update-input-description-field" value="${hourlyInfo.timedTask.descriptiveText}">
+                </div>
+                <div class="Id-Exp-storage">
+                    <div class="id-container">
+                        <label>Id:</label>
+                        <input type="text" class="update-input-id-field" value="${hourlyInfo.timedTask.id}">
+                    </div>
+                    <div class="exp-container">
+                        <label>Exp:</label>
+                        <input type="number" value="${hourlyInfo.timedTask.experiencePoints}">
+                    </div>
+                </div>
+                <div class="updater-button-container">
+                    <button class="confirm-update-button">Confirm</button>
+                    <button class="cancel-update-button">Cancel</button>
+                </div>
+            </div>
+        </div>
+    `;
+
+    const timeTableHTML = document.querySelector('.js-main-section-body').innerHTML;
+
+    document.querySelector('.js-main-section-body').innerHTML += updateHTML;
+    
+    document.querySelector('.cancel-update-button').addEventListener('click', () => {
+        document.querySelector('.js-main-section-body').innerHTML = timeTableHTML;
+        updateTimedTasks();
+    });
+
+
+
+};
